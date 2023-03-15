fcf44067e66d69a5bd3255cb70abd2495b9be1a0
Karolina Redden <dubajkaro@gmail.com>
2023-03-15 20:25:01 -0300

    fix a name in post for email

--- contactUs.php
+++ contactUs.php
@@ -9,12 +9,23 @@
 <body>
   <?php
   //get the data for the form, this version does not have error checking
-  $customeremail = $_POST["email"];
+  $customeremail = $_POST["customeremail"];
   $message = $_POST["message"];
   $replyWanted = false;
   if (isset($_POST["replayWanted"])) $replyWanted=true;
 
+  //build the text of the email, php has a method allowing to send email
+  $t = "You have received a message from " . $customeremail . " :\n";
+  $t = $t . $message . "\n";
+  if ($replyWanted)
+    $t = $t . "A reply was requested";
+  else 
+    $t = $t . "No reply was requested";
+  
+    #send email
+  mail("test@localhost", "Customer Message", $t);
 
+  echo "Thank you. Your message has been sent."
   ?>
   
 </body>
