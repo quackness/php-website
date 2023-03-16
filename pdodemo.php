59db14bae03f12fdcd00055d23e8d34e3491d672
Karolina Redden <dubajkaro@gmail.com>
2023-03-16 14:00:57 -0300

    build a query explicitely

--- pdodemo.php
+++ pdodemo.php
@@ -1,10 +1,20 @@
 <?php
 $searchtitle = "Girl";
-$searchautho = "Larsson";
+$searchauthor = "Larsson";
 try {
   $db = new PDO("mysql:host=localhost;dbname=library", "root_library", "rootpw");
 
   switch($argv[1]) {
+    //build a query explicitely
+    case 1:
+    $query = "select * from books where title like '%$searchtitle%' " . "and author like '%$searchauthor%'";
+    $stmt =$db->$query($query);
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+      printf("%-40s %-20s\n", $row["title"], $row["author"]);
+    }
+    break;
+
+
   
   }
 }
