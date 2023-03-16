c17805780944c0b9903b9f6dc77a6fd4edee8588
Karolina Redden <dubajkaro@gmail.com>
2023-03-16 13:54:54 -0300

    prepare statements starting code with pdo

---
+++ pdodemo.php
@@ -0,0 +1,10 @@
+<?php
+$searchtitle = "Girl";
+$searchautho = "Larsson";
+try {
+  $db = new PDO("mysql:host=localhost;dbname=library", "root_library", "rootpw");
+
+  switch($argv[1]) {
+  
+  }
+}
