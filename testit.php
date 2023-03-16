5d5d1b9a14c9a8992bea205bea175a02e584895e
Karolina Redden <dubajkaro@gmail.com>
2023-03-16 11:14:04 -0300

    add comments

--- testit.php
+++ testit.php
@@ -1,19 +1,21 @@
 <?php
-//accessa  db
-
-// $dbPassword = "rootpw";
-// $dbUserName = "root_library'";
-// $dbServer = "localhost";
-// $dbName = "library";
+//accessa  db, use try to be able to catch errors
 
 try {
-  // $db = new PDO('mysql:host='.$dbServer.';dbname='.$dbName, $dbUserName, $dbPassword);
+  //get a pdo class
   $db = new PDO("mysql:host=localhost;dbname=library", "root_library", "rootpw");
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //run the query and return a result set
   $sth = $db->query("select * from books where author like '%Bryson'");
+  //get the next row of the result set, return false is there are no more rows
   while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
     printf("%-40s %-20s\n", $row["title"], $row["author"]);
   }
+  //alt will be a foreach loop, info on printf https://www.w3schools.com/php/phptryit.asp?filename=tryphp_func_string_printf
+  // $query = "select * from books where author like '%Bryson'";
+  // foreach($db->query($query) as $row) {
+  //   printf("%s %s\n", $row["title"], $row["author"]);
+  // }
 }
 catch (PDOException $e) {
   printf("We had a problem: %s\n", $e->getMessage());
