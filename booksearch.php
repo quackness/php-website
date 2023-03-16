8d322bbccc2ab5c88faa289664ce737e85949355
Karolina Redden <dubajkaro@gmail.com>
2023-03-16 11:19:21 -0300

    add basic html skelton for the page

---
+++ booksearch.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Book Search Results</title>
+</head>
+<body>
+  <h3>Book Search Results</h3>
+  <?php
+
+  //this is a pdo version
+  ?>
+  
+</body>
+</html>
