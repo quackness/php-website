72635e51c04ab203493b3623df8f06c9666e19e2
Karolina Redden <dubajkaro@gmail.com>
2023-03-15 21:25:51 -0300

    add a check for the email validation with regex,todo: there is still a warning in a browser which needs to be fixed, most likely the issue is related with character escaping possible solution at https://mantisbt.org/bugs/view.php?id=13051

--- contactUs2.php
+++ contactUs2.php
@@ -13,6 +13,36 @@
     echo "You did not enter an email address";
     exit;
   }
+  $customeremail = $_POST["customeremail"];
+  // if (! ereg("[a-z]+@[a-z]+\.[a-z]+", $_POST["customeremail"])) {
+  //   echo "Email address is not valid";
+  //   exit;
+  // }
+
+  if (!preg_match("~([a-zA-Z0-9!#$%&'*+-/=?^_`{|}~])@([a-zA-Z0-9-]).([a-zA-Z0-9]{2,4})~", 
+  $_POST["customeremail"])) {
+    echo 'This is an invalid email.';
+    exit;
+  }
+
+  //get the data for the form, this version does not have error checking
+  // $customeremail = $_POST["customeremail"];
+  $message = $_POST["message"];
+  $replyWanted = false;
+  if (isset($_POST["replayWanted"])) $replyWanted=true;
+
+  //build the text of the email, php has a method allowing to send email
+  $t = "You have received a message from " . $customeremail . " :\n";
+  $t = $t . $message . "\n";
+  if ($replyWanted)
+    $t = $t . "A reply was requested";
+  else 
+    $t = $t . "No reply was requested";
+  
+    #send email
+  mail("dub@localhost", "Customer Message", $t);
+
+  echo "Thank you. Your message has been sent."
 
 
 
