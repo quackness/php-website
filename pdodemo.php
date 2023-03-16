bc623252618ca05642aa99e8fb987fea13faaf95
Karolina Redden <dubajkaro@gmail.com>
2023-03-16 14:26:28 -0300

    test with a title that has ' character, it throws an error due to issues with escaping

--- pdodemo.php
+++ pdodemo.php
@@ -1,30 +1,30 @@
 <?php
-$searchtitle = "Dodger";
-$searchauthor = "Terry T.";
+$searchtitle = "The lost's";
+$searchauthor = "Bill Bryson";
 try {
   $db = new PDO("mysql:host=localhost;dbname=library", "root_library", "rootpw");
 
-  switch($argv[1]) {
-    //build a query explicitely
-    case 1:
-    $query = "select * from books where title like '%$searchtitle%' " . "and author like '%$searchauthor%'";
-    $stmt =$db->$query($query);
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-      printf("%-40s %-20s\n", $row["title"], $row["author"]);
-    }
-    break;
-
-    case 2:
-      //use a prepeared statement with paramaters bound by postion - method 1
-      $stmt=$db->prepare("select * from books where title like ? and author like ?");
-      $stmt->execute(array("$searchtitle", "$searchauthor"));
+  // switch ($argv[1]) {
+  //   //build a query explicitely, not the best option since it has problems with escaping characters like '
+  //   case 1:
+      $query = "select * from books where title like '%$searchtitle%' " . "and author like '%$searchauthor%'";
+      $stmt = $db->query($query);
       while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         printf("%-40s %-20s\n", $row["title"], $row["author"]);
       }
-    break;
-
-
-  
-  }
+      // break;
+    }
+    // case 2:
+      //use a prepeared statement with paramaters bound by postion - method 1
+  //     $stmt = $db->prepare("select * from books where title like ? and author like ?");
+  //     $stmt->execute(array("$searchtitle", "$searchauthor"));
+  //     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+  //       printf("%-40s %-20s\n", $row["title"], $row["author"]);
+  //     }
+  //     break;
+  // }
+// } 
+catch (PDOException $e) {
+  printf("We had a problem: %s\n", $e->getMessage());
 }
 ?>
