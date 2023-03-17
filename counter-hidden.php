ea181c0eb499f2b0786e9ed078e520cb6b251c38
Karolina Redden <dubajkaro@gmail.com>
2023-03-17 11:10:46 -0300

    format

--- counter-hidden.php
+++ counter-hidden.php
@@ -1,20 +1,23 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
+
 <head>
   <title>Counting with a hidden field</title>
 </head>
+
 <body>
   <FORM action="counter-hidden.php" method="GET">
-  <INPUT type="submit" name="Count" value="Count">
-  <?php
-   if (! isset($_GET['hiddencounter']))
+    <INPUT type="submit" name="Count" value="Count">
+    <?php
+    if (!isset($_GET['hiddencounter']))
       $count = 0;
-   else
+    else
       $count = $_GET['hiddencounter'];
-   $count = $count + 1;
-   echo '<input type="hidden" value=' . $count . ' name="hiddencounter">';
-   echo "count is $count";
-  ?>
+    $count = $count + 1;
+    echo '<input type="hidden" value=' . $count . ' name="hiddencounter">';
+    echo "count is $count";
+    ?>
   </FORM>
 </body>
+
 </html>
