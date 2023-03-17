252d0262dba6b872e63e605450d653d3ff79de63
Karolina Redden <dubajkaro@gmail.com>
2023-03-17 12:42:00 -0300

    implement a colour chooser form

---
+++ colour-chooser.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<?php
+  // Build the array of colour choices
+  $colours = array("Pink"   => "f0d0d0",
+                   "Violet" => "cda8ef",
+                   "Blue"   => "a8c1ef",
+                   "Green"  => "a8efab",
+                   "Yellow" => "efee7b" );
+
+  // If this is a postback, create the cookie
+  if (isset($_GET['colourchosen'])) {
+    setcookie('colourpreference', $colours[$_GET['colourchosen']], time() + 24*3600, "/", "");
+  }
+?>
+<html>
+<head>
+  <title>Choose Colour Preference</title>
+</head>
+<body>
+<?php
+  if (isset($_GET['colourchosen'])) {
+    // This is the postback so just thank the user
+    echo "Your colour preference has been recorded";
+    exit;
+  }
+  // Not a postback, so present the colour selection form:
+?>
+<form action="colour-chooser.php" method="GET">
+<table>
+  <tbody>
+    <tr>
+      <td>Choose your colour:</td>
+      <td>
+        <select size="1" name="colourchosen">
+          <?php
+          // Populate drop-down from array of colour choices
+          foreach ($colours as $name => $hex)
+            echo "<option> " . $name;
+          ?>
+        </select>
+      </td>
+    </tr>
+    <tr>
+      <td colspan = "2">
+        <INPUT type="submit" name="Confirm" value="Confirm Preference"></td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
+</form>
+</body>
+</html>
